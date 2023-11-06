3225de4651f3e5a088878d5877de357259c2fdc1
Cairo Noleto <caironoleto@gmail.com>
2023-11-06 15:16:44 -0300

    Revert "updates elixir LSP to elixir-tools/elixir-tools.nvim"
    This reverts commit c6141a317ce02e7a47dc18acca4ebd9b40f95348.

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -68,11 +68,20 @@ local on_attach = function(_, bufnr)
 end
 
 -- Configure ElixirLS as the LSP server for Elixir.
-require("elixir").setup({
-  nextls = {enable = true},
-  credo = {enable = true},
-  elixirls = {enable = true},
-})
+require'lspconfig'.elixirls.setup{
+  cmd = { "/opt/homebrew/Cellar/elixir-ls/0.13.0/libexec/language_server.sh" },
+  capabilities = capabilities,
+  on_attach = on_attach,
+  flags = {
+    debounce_text_changes = 150,
+  },
+  elixirLS = {
+    dialyzerEnabled = true,
+    fetchDeps = false,
+    enableTestLenses = true,
+    suggestSpecs = false,
+  };
+}
 
 -- Configure pylsp as the LSP server for Python.
 -- require("lspconfig").pylsp.setup{
