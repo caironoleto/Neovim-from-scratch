b0dc1fa747095438e37b5fc50f6880263b79bab4
Cairo Noleto <hey@caironoleto.dev>
2024-05-05 10:55:46 -0400

    feat: add support to chat gpt inside nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -137,6 +137,45 @@ return packer.startup(function(use)
   --   end
   -- }
 
+  -- ChatGPT
+  use({
+    "jackMort/ChatGPT.nvim",
+      config = function()
+        require("chatgpt").setup({
+          keymaps = {
+            close = "<C-c>",
+            yank_last = "<C-y>",
+            yank_last_code = "<C-k>",
+            scroll_up = "<C-u>",
+            scroll_down = "<C-d>",
+            new_session = "<C-n>",
+            cycle_windows = "<Tab>",
+            cycle_modes = "<C-f>",
+            next_message = "<C-j>",
+            prev_message = "<C-k>",
+            select_session = "<C-Space>",
+            rename_session = "r",
+            delete_session = "d",
+            draft_message = "<C-r>",
+            edit_message = "e",
+            delete_message = "d",
+            toggle_settings = "<C-o>",
+            toggle_sessions = "<C-p>",
+            toggle_help = "<C-h>",
+            toggle_message_role = "<C-r>",
+            toggle_system_role_open = "<C-s>",
+            stop_generating = "<C-x>",
+          },
+        })
+      end,
+      requires = {
+        "MunifTanjim/nui.nvim",
+        "nvim-lua/plenary.nvim",
+        "folke/trouble.nvim",
+        "nvim-telescope/telescope.nvim"
+      }
+  })
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
