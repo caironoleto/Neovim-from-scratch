da35c7f8ce3b645722d895f4e4f5af2240bc3cbd
Cairo Noleto <hey@caironoleto.dev>
2024-07-16 08:23:54 -0400

    feat: add more options to telescope

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -3,6 +3,12 @@ if not status_ok then
   return
 end
 
+
+local status_ok,lga_actions = pcall(require, "telescope-live-grep-args.actions")
+if not status_ok then
+  return
+end
+
 local actions = require "telescope.actions"
 
 telescope.setup {
@@ -92,5 +98,21 @@ telescope.setup {
     --   extension_config_key = value,
     -- }
     -- please take a look at the readme of the extension you want to configure
+    live_grep_args = {
+      auto_quoting = true, -- enable/disable auto-quoting
+      -- define mappings, e.g.
+      mappings = { -- extend mappings
+        i = {
+          ["<C-k>"] = lga_actions.quote_prompt(),
+          ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+          -- freeze the current list and start a fuzzy search in the frozen list
+          ["<C-space>"] = actions.to_fuzzy_refine,
+        },
+      },
+      -- ... also accepts theme settings, for example:
+      -- theme = "dropdown", -- use dropdown theme
+      -- theme = { }, -- use own theme spec
+      -- layout_config = { mirror=true }, -- mirror preview pane
+    }
   },
 }
