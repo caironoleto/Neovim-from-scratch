5a807794761cf8104d1f00edbe2100ab54a7c25c
Cairo Noleto <hey@caironoleto.dev>
2024-10-21 20:12:40 -0400

    feat: add python to treesitter

--- lua/user/nvim-treesitter.lua
+++ lua/user/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "lua", "vim", "vimdoc", "query", "elixir", "typescript", "javascript", "heex", "eex" },
+  ensure_installed = { "lua", "vim", "vimdoc", "query", "elixir", "typescript", "javascript", "heex", "eex", "python" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
